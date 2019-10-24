f01789f9bc24ed6bdee2f002eb941c09ead07741
Matza <cmatza@amazon.com>
2019-10-24 15:45:53 -0700

    Inadvertently merged one commit ahead of the desired merge branch
    This reverts commit 09814df65530e89510b008a4eb33a7d18cc69a41.

--- guitest.py
+++ guitest.py
@@ -13,17 +13,8 @@ class Player:
     def __init__(self, mark, player):
         self._mark = mark
         self._player = player
-class Board:
-    def __init__(self):
-        self.value = ["-","-","-","-","-","-","-","-","-"]
-    @property
-    def value(self):
-        return self._value
-    @value.setter
-    def value(self, update):
-        self._value = update
 ## Global board state, player state, and scores - this is a target for refactoring. 
-BoardValue = Board()
+BoardValue = ["-","-","-","-","-","-","-","-","-"]
 player1 = {"mark": "X", "player":"human"}
 player2 = {"mark": "O", "player": "cpu"}
 ai_on = True
@@ -70,6 +61,7 @@ class FirstPlayerDialog:
         except:
             value = 50
         return value
+
     ## Tests the player's guess against a random number 1/100. A second number 1/100 is generated for the "computer's guess"
     def determine_first_player(self, player_guess):
         global cur_play
@@ -101,19 +93,17 @@ class FirstPlayerDialog:
 
 class BoardButton():
     ##Creates a board button in a given position. Cleverly here - it appends to the global btn array to determine it's own position.
-    def __init__(self,row_frame,b, btn, board):
+    def __init__(self,row_frame,b):
+        global btn
         self.position= len(btn)
-        btn.append(Button(row_frame, text=b, relief=GROOVE, width=2,command=lambda: self.callPlayMove(btn, BoardValue.value)))
+        btn.append(Button(row_frame, text=b, relief=GROOVE, width=2,command=lambda: self.callPlayMove()))
         btn[self.position].pack(side="left")
 
     ##Just a wrapper for the play move function.
 
-    def callPlayMove(self, btn, board):
-        global BoardValue
-        new_board = PlayMove(self.position, btn, board)
-        BoardValue.value = new_board
-
-        UpdateBoard(btn, BoardValue.value)
+    def callPlayMove(self):
+        global scores
+        PlayMove(self.position)
 
 ##Draws the Score Board. Originally intended as a class implementation.
 
@@ -130,26 +120,26 @@ def ScoreFrame(parent):
     o_score_value = Label(oframe, textvariable=o_score).pack(side="right")
 
 
-def DrawBoard(button, board):
+def DrawBoard():
+    global BoardValue
     global cur_play
     global x_score
     global o_score
 
     ##Creates a new BoardButton for every cell in BoardValue, creates a new Frame whenever we've added 3 buttons
 
-    for i, b in enumerate(board):
+    for i, b in enumerate(BoardValue):
+        global btn
         if i%3 == 0:
             row_frame = Frame(window)
             row_frame.pack(side="top")
-        BoardButton(row_frame,b, button, board)
+        BoardButton(row_frame,b)
     ScoreFrame(window)
 
     ## ai_move() call here in case the ai goes first
-
+    
     if ai_on:
-        new_board = ai_move(BoardValue.value)
-        board = new_board
-        UpdateBoard(button, BoardValue.value)
+        ai_move()
         #btn.append(Button(row_frame, text=b, relief=GROOVE, width=2))
         #btn[i].pack(side="left")
 def compatibility_flip_player(cur_play, player1, player2):
@@ -180,10 +170,11 @@ def compatibility_playerdicts_to_tuples(cur_play, player1, player2):
     return { "cur_play": cur_play_tuple, "player1": player1_tuple, "player2": player2_tuple}
 
 
-def ai_move(board):
+def ai_move():
     global cur_play
     global player1
     global player2
+    global BoardValue
 
     ##Ensures that it's the AI's turn, this is important because it lets you call ai_move() from wherever you want
     ##in the event loop, without giving the AI extra turns.
@@ -191,29 +182,26 @@ def ai_move(board):
         print("AI MOVING")
         ##Calls the minimax optimizer from tictactoe.py and makes a move. (Modifies board state)
         compatibility_players = compatibility_playerdicts_to_tuples(cur_play, player1, player2)
-
-        new_board = copy.deepcopy(board)
-        new_board[minimax(new_board, compatibility_players["cur_play"], compatibility_players["player1"], compatibility_players["player2"])[1]] = cur_play['mark']
+        BoardValue[minimax(BoardValue, compatibility_players["cur_play"], compatibility_players["player1"], compatibility_players["player2"])[1]] = cur_play['mark']
 
         ##Flips the player giving control back to the player.
 
         cur_play = compatibility_flip_player(cur_play, player1, player2)
         print("END AI MOVE")
-        return new_board
     else:
-        return board
+        return
 
 ##For every button in array, set the button text to the board state value.
-def UpdateBoard(btn, board):
-    for i, b in enumerate(board):
+def UpdateBoard():
+    for i, b in enumerate(BoardValue):
+        global btn
         btn[i].config(text=b)
-        btn[i].board = board
-    check_winner(btn, board)
     ##Updating the board automatically updates variable displays. (The scoreboard)
     window.update_idletasks
 
-def PlayMove(position_clicked, btn, board):
+def PlayMove(position_clicked):
     ##Lots of global state here, global state bad.
+    global BoardValue
     global cur_play
     global player1
     global player2
@@ -223,57 +211,63 @@ def PlayMove(position_clicked, btn, board):
     global scores
 
 
-    if board[position_clicked] == '-':
+    if BoardValue[position_clicked] == '-':
         print("PLAYER BLOCK")
         ##Makes the move by updating board state
-        new_board = copy.deepcopy(board)
-        new_board[position_clicked] = cur_play['mark']
+        BoardValue[position_clicked] = cur_play['mark']
 
         ##Flips current player between player 1 and player 2
         cur_play = compatibility_flip_player(cur_play, player1, player2)
 
-        if ai_on and not check_for_winner(new_board) and not check_if_tie(new_board):
-            new_board = ai_move(new_board)
-
-        return new_board
+        if ai_on and not check_for_winner(BoardValue) and not check_if_tie(BoardValue):
+            ai_move()
+            UpdateBoard()
     else:
         tkinter.messagebox.showinfo("Tic-Tac-Toe", "Move Already Played")
-        return board
 
     ##Checks for our winner, updates score human/cpu, and asks if the player would like to play again.
-def check_winner(button, board):
-    global BoardValue
-    winner = check_for_winner(board)
-    tie = check_if_tie(board)
-    if winner or tie:
-        outstring = str(winner) + " Wins! Play again?"
+
+    winner = check_for_winner(BoardValue)
+    tie = check_if_tie(BoardValue)
+    if winner:
         if player1['mark'] == winner:
             scores[player1['player']] += 1
-        elif player2['mark'] == winner:
-            scores[player2['player']] += 1
         else:
-            outstring = "Tie Game! Play again?"
+            scores[player2['player']] += 1
         x_score.set(str(scores[player1['player']]))
         o_score.set(str(scores[player2['player']]))
 
         ##Creates a dialog box asking if the user would like to play again
-        retry = tkinter.messagebox.askquestion("Tic Tac Toe", outstring)
+        retry = tkinter.messagebox.askquestion("Tic Tac Toe", winner + " Wins! Play again?")
         if retry == 'yes':
-            BoardValue = Board()
+            BoardValue = ["-","-","-","-","-","-","-","-","-"]
             dialog = FirstPlayerDialog(window)
+
             #Wait_window disrupts the main event loop until the created window is destroyed.
             window.wait_window(dialog.top)
-            BoardValue.value = ai_move(BoardValue.value)
-            UpdateBoard(button, BoardValue.value)
+            ai_move()
+            UpdateBoard()
             window.update()
 
         else:
             #Closes the application
             window.destroy()
+    elif tie:
+        ##Same thing as win condition, except without updating the score. 
 
+        retry = tkinter.messagebox.askquestion("Tic Tac Toe", "Tie Game, play again?")
+        if retry == 'yes':
+            BoardValue = ["-","-","-","-","-","-","-","-","-"]
+            dialog = FirstPlayerDialog(window)
+            window.wait_window(dialog.top)
+            ai_move()
+            UpdateBoard()
+        else:
+            window.destroy()
+        print("END PLAYER BLOCK")
 dialog = FirstPlayerDialog(window)
 window.lift()
 window.attributes()
 window.wait_window(dialog.top)
-DrawBoard(btn, BoardValue.value)
+DrawBoard()
 window.mainloop()
